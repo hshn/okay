f33c8b616926129125a2cee5cd25d65f32852d30
Shota Hoshino <sht.hshn@gmail.com>
2022-09-12 23:44:14 +0900

    refactor(test): remove munit

--- core/src/test/scala/okay/ViolationsSpec.scala
+++ core/src/test/scala/okay/ViolationsSpec.scala
@@ -1,69 +1,75 @@
 package okay
 
-import munit.FunSuite
 import okay.Violations.Path
+import zio.Scope
+import zio.test.Spec
+import zio.test.TestEnvironment
+import zio.test.ZIOSpecDefault
+import zio.test.assertTrue
 
-class ViolationsSpec extends FunSuite {
-  test("++ merges two Violations recursively") {
-    val a = Violations(
-      values = Seq("a", "b"),
-      children = Map(
-        Path.Key("c") -> Violations(
-          values = Seq("d"),
+class ViolationsSpec extends ZIOSpecDefault {
+  override def spec: Spec[TestEnvironment with Scope, Any] = suite("Violations")(
+    test("++ merges two Violations recursively") {
+      val a = Violations(
+        values = Seq("a", "b"),
+        children = Map(
+          Path.Key("c") -> Violations(
+            values = Seq("d"),
+          ),
+          Path.Key("e") -> Violations(
+            values = Seq("f"),
+          ),
+          Path.Index(1) -> Violations(
+            values = Seq("g"),
+          ),
+          Path.Index(2) -> Violations(
+            values = Seq("h"),
+          ),
         ),
-        Path.Key("e") -> Violations(
-          values = Seq("f"),
+      )
+      val b = Violations(
+        values = Seq("b", "c"),
+        children = Map(
+          Path.Key("e") -> Violations(
+            values = Seq("f"),
+          ),
+          Path.Key("g") -> Violations(
+            values = Seq("h"),
+          ),
+          Path.Index(2) -> Violations(
+            values = Seq("i"),
+          ),
+          Path.Index(3) -> Violations(
+            values = Seq("j"),
+          ),
         ),
-        Path.Index(1) -> Violations(
-          values = Seq("g"),
-        ),
-        Path.Index(2) -> Violations(
-          values = Seq("h"),
-        ),
-      ),
-    )
-    val b = Violations(
-      values = Seq("b", "c"),
-      children = Map(
-        Path.Key("e") -> Violations(
-          values = Seq("f"),
-        ),
-        Path.Key("g") -> Violations(
-          values = Seq("h"),
-        ),
-        Path.Index(2) -> Violations(
-          values = Seq("i"),
-        ),
-        Path.Index(3) -> Violations(
-          values = Seq("j"),
-        ),
-      ),
-    )
+      )
 
-    val c = Violations(
-      values = Seq("a", "b", "b", "c"),
-      children = Map(
-        Path.Key("c") -> Violations(
-          values = Seq("d"),
+      val c = Violations(
+        values = Seq("a", "b", "b", "c"),
+        children = Map(
+          Path.Key("c") -> Violations(
+            values = Seq("d"),
+          ),
+          Path.Key("e") -> Violations(
+            values = Seq("f", "f"),
+          ),
+          Path.Key("g") -> Violations(
+            values = Seq("h"),
+          ),
+          Path.Index(1) -> Violations(
+            values = Seq("g"),
+          ),
+          Path.Index(2) -> Violations(
+            values = Seq("h", "i"),
+          ),
+          Path.Index(3) -> Violations(
+            values = Seq("j"),
+          ),
         ),
-        Path.Key("e") -> Violations(
-          values = Seq("f", "f"),
-        ),
-        Path.Key("g") -> Violations(
-          values = Seq("h"),
-        ),
-        Path.Index(1) -> Violations(
-          values = Seq("g"),
-        ),
-        Path.Index(2) -> Violations(
-          values = Seq("h", "i"),
-        ),
-        Path.Index(3) -> Violations(
-          values = Seq("j"),
-        ),
-      ),
-    )
+      )
 
-    assertEquals(a ++ b, c)
-  }
+      assertTrue(a ++ b == c)
+    },
+  )
 }
