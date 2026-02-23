0f2ab06481e62749d8803c69a1fdafca4345d8e4
Shota Hoshino <sht.hshn@gmail.com>
2026-02-23 16:26:13 +0900

    style: apply scalafmt

--- core/src/main/scala/okay/syntax/StandardSyntax.scala
+++ core/src/main/scala/okay/syntax/StandardSyntax.scala
@@ -1,9 +1,8 @@
 package okay.syntax
 
 import okay.Violations
-import zio.ZIO
-
 import scala.language.implicitConversions
+import zio.ZIO
 
 trait StandardSyntax extends TupleZValidatedSyntax {
   implicit def syntaxCanBeValidated[A](a: A): CanBeValidatedOps[A]                                = new CanBeValidatedOps[A](a)
