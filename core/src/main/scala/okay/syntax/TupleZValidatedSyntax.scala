d9eb9310559f806ff724bcdc869e57a4ba9b744a
Shota Hoshino <sht.hshn@gmail.com>
2026-02-24 10:59:11 +0900

    feat: replace TupleOperationGenerator with typeclass-based tuple validation (#76)

---
+++ core/src/main/scala/okay/syntax/TupleZValidatedSyntax.scala
@@ -0,0 +1,34 @@
+package okay.syntax
+
+import cats.data.ValidatedNec
+import cats.syntax.all.*
+import okay.Violations
+import zio.ZIO
+
+trait TupleZValidatedSyntax:
+  extension [R, V, A](validation: ZIO[R, Violations[V], A])
+    def validateN[B](f: A => B): ZIO[R, Violations[V], B] =
+      validation.map(f)
+
+  extension [R, V, T <: Tuple, Out <: Tuple](validations: T)(using tv: ValidateTuple[R, V, T, Out])
+    def validateN[A](f: Out => A): ZIO[R, Violations[V], A] =
+      tv.validate(validations)
+        .map(_.map(f).leftMap(_.combineAll).toEither)
+        .absolve
+
+sealed trait ValidateTuple[R, V, T <: Tuple, Out <: Tuple]:
+  def validate(t: T): ZIO[R, Nothing, ValidatedNec[Violations[V], Out]]
+
+object ValidateTuple:
+  given empty[R, V]: ValidateTuple[R, V, EmptyTuple, EmptyTuple] with
+    def validate(t: EmptyTuple): ZIO[R, Nothing, ValidatedNec[Violations[V], EmptyTuple]] =
+      ZIO.succeed(EmptyTuple.validNec)
+
+  given cons[R, V, H, T <: Tuple, TOut <: Tuple](using
+    tail: ValidateTuple[R, V, T, TOut],
+  ): ValidateTuple[R, V, ZIO[R, Violations[V], H] *: T, H *: TOut] with
+    def validate(t: ZIO[R, Violations[V], H] *: T): ZIO[R, Nothing, ValidatedNec[Violations[V], H *: TOut]] =
+      for
+        headResult <- t.head.either.map(_.toValidatedNec)
+        tailResult <- tail.validate(t.tail)
+      yield (headResult, tailResult).mapN(_ *: _)
