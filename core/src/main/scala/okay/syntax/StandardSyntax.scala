742fa18f497b79297faf37fc901a9163c5ec9f21
Shota Hoshino <sht.hshn@gmail.com>
2026-02-26 00:26:59 +0900

    refactor: move ValidateAsPartiallyApplied into StandardSyntax.scala

--- core/src/main/scala/okay/syntax/StandardSyntax.scala
+++ core/src/main/scala/okay/syntax/StandardSyntax.scala
@@ -1,5 +1,6 @@
 package okay.syntax
 
+import okay.Validation
 import okay.Violations
 import zio.ZIO
 
@@ -9,3 +10,12 @@ trait StandardSyntax extends TupleZValidatedSyntax:
     def at(path: Violations.Path): ZIO[R, Violations[V], A] = value.mapError(_.asChild(path))
     def at(key: String): ZIO[R, Violations[V], A]           = at(Violations.Path.Key(key))
     def at(index: Int): ZIO[R, Violations[V], A]            = at(Violations.Path.Index(index))
+
+final class ValidateAsPartiallyApplied[A, B](private val a: A):
+  def apply[R, V](using validation: Validation[R, V, A, B]): ZIO[R, Violations[V], B]                     = validation.run(a)
+  def at[R, V](path: Violations.Path)(using validation: Validation[R, V, A, B]): ZIO[R, Violations[V], B] =
+    validation.run(a).mapError(_.asChild(path))
+  def at[R, V](key: String)(using validation: Validation[R, V, A, B]): ZIO[R, Violations[V], B] =
+    at(Violations.Path.Key(key))
+  def at[R, V](index: Int)(using validation: Validation[R, V, A, B]): ZIO[R, Violations[V], B] =
+    at(Violations.Path.Index(index))
