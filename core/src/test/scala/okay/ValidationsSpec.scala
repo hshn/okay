b7f5eca5d59c784b132ab07d163fca178e00926b
Shota Hoshino <sht.hshn@gmail.com>
2026-02-27 00:54:12 +0900

    chore: modernize ZIO Test to suiteAll + assertTrue style (#92)

--- core/src/test/scala/okay/ValidationsSpec.scala
+++ core/src/test/scala/okay/ValidationsSpec.scala
@@ -2,103 +2,87 @@ package okay
 
 import okay.defaults.{given, *}
 import okay.defaults.Validations
-import zio.Scope
-import zio.test.Assertion.*
-import zio.test.Spec
-import zio.test.TestEnvironment
-import zio.test.ZIOSpecDefault
-import zio.test.assertZIO
+import zio.test.*
 
 object ValidationsSpec extends ZIOSpecDefault {
-  val requiredSuite = suite("required() can extract from Option")(
-    test("success") {
-      val validation = Validations.required[String]
+  override def spec = suiteAll("Validations") {
+    suiteAll("required() can extract from Option") {
+      test("success") {
+        val validation = Validations.required[String]
 
-      assertZIO(validation.run(Some("hello")))(equalTo("hello"))
-    },
-    test("failure") {
-      val validation = Validations.required[String]
+        for result <- validation.run(Some("hello"))
+        yield assertTrue(result == "hello")
+      }
+      test("failure") {
+        val validation = Validations.required[String]
 
-      assertZIO(validation.run(None).either)(
-        isLeft(equalTo(Violations.single(Violation.Required))),
-      )
-    },
-  )
+        for result <- validation.run(None).either
+        yield assertTrue(result.is(_.left) == Violations.single(Violation.Required))
+      }
+    }
+    suiteAll("parseInt() can parse string to int") {
+      test("success") {
+        val validation = Validations.parseInt
 
-  val parseIntSuite = suite("parseInt() can parse string to int")(
-    test("success") {
-      val validation = Validations.parseInt
+        for result <- validation.run("42")
+        yield assertTrue(result == 42)
+      }
+      test("failure") {
+        val validation = Validations.parseInt
 
-      assertZIO(validation.run("42"))(equalTo(42))
-    },
-    test("failure") {
-      val validation = Validations.parseInt
+        for result <- validation.run("abc").either
+        yield assertTrue(result.is(_.left) == Violations.single(Violation.NonIntegerString("abc")))
+      }
+    }
+    suiteAll("maxLength() can test string length") {
+      test("success") {
+        val validation = Validations.maxLength(max = 4)
+        val value      = "a".repeat(4)
 
-      assertZIO(validation.run("abc").either)(
-        isLeft(equalTo(Violations.single(Violation.NonIntegerString("abc")))),
-      )
-    },
-  )
+        for result <- validation.run(value)
+        yield assertTrue(result == value)
+      }
+      test("failure") {
+        val validation = Validations.maxLength(max = 4)
+        val value      = "a".repeat(5)
 
-  val maxLengthSuite = suite("maxLength() can test string length")(
-    test("success") {
-      val validation = Validations.maxLength(max = 4)
-      val value      = "a".repeat(4)
+        for result <- validation.run(value).either
+        yield assertTrue(result.is(_.left) == Violations.single(Violation.TooLongString(value, maxLength = 4)))
+      }
+    }
+    suiteAll("minLength() can test string length") {
+      test("success") {
+        val validation = Validations.minLength(min = 4)
+        val value      = "a".repeat(4)
 
-      assertZIO(validation.run(value))(equalTo(value))
-    },
-    test("failure") {
-      val validation = Validations.maxLength(max = 4)
-      val value      = "a".repeat(5)
+        for result <- validation.run(value)
+        yield assertTrue(result == value)
+      }
+      test("failure") {
+        val validation = Validations.minLength(min = 4)
+        val value      = "a".repeat(3)
 
-      assertZIO(validation.run(value).either)(
-        isLeft(equalTo(Violations.single(Violation.TooLongString(value, maxLength = 4)))),
-      )
-    },
-  )
+        for result <- validation.run(value).either
+        yield assertTrue(result.is(_.left) == Violations.single(Violation.TooShortString(value, minLength = 4)))
+      }
+    }
+    suiteAll("matches() can test with regex") {
+      test("success") {
+        val pattern    = "^abc$".r
+        val validation = Validations.matches(pattern)
+        val value      = "abc"
 
-  val minLengthSuite = suite("minLength() can test string length")(
-    test("success") {
-      val validation = Validations.minLength(min = 4)
-      val value      = "a".repeat(4)
+        for result <- validation.run(value)
+        yield assertTrue(result == value)
+      }
+      test("failure") {
+        val pattern    = "^abc$".r
+        val validation = Validations.matches(pattern)
+        val value      = "abcd"
 
-      assertZIO(validation.run(value))(equalTo(value))
-    },
-    test("failure") {
-      val validation = Validations.minLength(min = 4)
-      val value      = "a".repeat(3)
-
-      assertZIO(validation.run(value).either)(
-        isLeft(equalTo(Violations.single(Violation.TooShortString(value, minLength = 4)))),
-      )
-    },
-  )
-
-  val matchesSuite = suite("matches() can test with regex")(
-    test("success") {
-      val pattern    = "^abc$".r
-      val validation = Validations.matches(pattern)
-      val value      = "abc"
-
-      assertZIO(validation.run(value).either)(
-        isRight(equalTo(value)),
-      )
-    },
-    test("failure") {
-      val pattern    = "^abc$".r
-      val validation = Validations.matches(pattern)
-      val value      = "abcd"
-
-      assertZIO(validation.run(value).either)(
-        isLeft(equalTo(Violations.single(Violation.Unmatched(value, pattern)))),
-      )
-    },
-  )
-  override def spec: Spec[TestEnvironment with Scope, Any] = suite("Validations")(
-    requiredSuite,
-    parseIntSuite,
-    minLengthSuite,
-    maxLengthSuite,
-    matchesSuite,
-  )
+        for result <- validation.run(value).either
+        yield assertTrue(result.is(_.left) == Violations.single(Violation.Unmatched(value, pattern)))
+      }
+    }
+  }
 }
