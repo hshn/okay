38d8d4be1ed3bbe3c4e69d275d8e833cfe5ff207
Shota Hoshino <sht.hshn@gmail.com>
2026-04-15 19:34:17 +0900

    replace ZIO with Either in ValidateN

--- core/src/main/scala/yoshi/syntax/ValidateN.scala
+++ core/src/main/scala/yoshi/syntax/ValidateN.scala
@@ -1,9 +1,8 @@
 package yoshi.syntax
 
 import yoshi.Violations
-import zio.ZIO
 
-/** Provides `.validateN(f)` for parallel validation of multiple ZIO effects.
+/** Provides `.validateN(f)` for parallel validation of multiple `Either` values.
   *
   * {{{
   * (
@@ -13,33 +12,29 @@ import zio.ZIO
   * }}}
   */
 trait ValidateN:
-  extension [R, V, A](validation: ZIO[R, Violations[V], A])
-    def validateN[B](f: A => B): ZIO[R, Violations[V], B] =
+  extension [V, A](validation: Either[Violations[V], A])
+    def validateN[B](f: A => B): Either[Violations[V], B] =
       validation.map(f)
 
-  extension [R, V, T <: Tuple, Out <: Tuple](validations: T)(using tv: ValidateTuple[R, V, T, Out])
-    def validateN[A](f: Out => A): ZIO[R, Violations[V], A] =
-      tv.validate(validations)
-        .map(_.map(f))
-        .absolve
+  extension [V, T <: Tuple, Out <: Tuple](validations: T)(using tv: ValidateTuple[V, T, Out])
+    def validateN[A](f: Out => A): Either[Violations[V], A] =
+      tv.validate(validations).map(f)
 
-sealed trait ValidateTuple[R, V, T <: Tuple, Out <: Tuple]:
-  def validate(t: T): ZIO[R, Nothing, Either[Violations[V], Out]]
+sealed trait ValidateTuple[V, T <: Tuple, Out <: Tuple]:
+  def validate(t: T): Either[Violations[V], Out]
 
 object ValidateTuple:
-  given empty[R, V]: ValidateTuple[R, V, EmptyTuple, EmptyTuple] with
-    def validate(t: EmptyTuple): ZIO[R, Nothing, Either[Violations[V], EmptyTuple]] =
-      ZIO.succeed(Right(EmptyTuple))
+  given empty[V]: ValidateTuple[V, EmptyTuple, EmptyTuple] with
+    def validate(t: EmptyTuple): Either[Violations[V], EmptyTuple] =
+      Right(EmptyTuple)
 
-  given cons[R, V, H, T <: Tuple, TOut <: Tuple](using
-    tail: ValidateTuple[R, V, T, TOut],
-  ): ValidateTuple[R, V, ZIO[R, Violations[V], H] *: T, H *: TOut] with
-    def validate(t: ZIO[R, Violations[V], H] *: T): ZIO[R, Nothing, Either[Violations[V], H *: TOut]] =
-      t.head.either
-        .zipPar(tail.validate(t.tail))
-        .map {
-          case (Right(h), Right(t)) => Right(h *: t)
-          case (Left(e1), Left(e2)) => Left(e1 ++ e2)
-          case (Left(e), _)         => Left(e)
-          case (_, Left(e))         => Left(e)
-        }
+  given cons[V, H, T <: Tuple, TOut <: Tuple](using
+    tail: ValidateTuple[V, T, TOut],
+  ): ValidateTuple[V, Either[Violations[V], H] *: T, H *: TOut] with
+    def validate(t: Either[Violations[V], H] *: T): Either[Violations[V], H *: TOut] =
+      (t.head, tail.validate(t.tail)) match {
+        case (Right(h), Right(t)) => Right(h *: t)
+        case (Left(e1), Left(e2)) => Left(e1 ++ e2)
+        case (Left(e), _)         => Left(e)
+        case (_, Left(e))         => Left(e)
+      }
